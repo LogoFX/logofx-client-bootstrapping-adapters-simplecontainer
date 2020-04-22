1b208c36d063f92f1d00a939e61ac916cc878a23
Gennady Verdel <godrose@gmail.com>
2020-04-22 11:10:54 +0300

    fix: removed unsupported scenarios

--- src/LogoFX.Client.Bootstrapping.Adapters.SimpleContainer.Tests/ExtendedSimpleContainerAdapterTests.cs
+++ src/LogoFX.Client.Bootstrapping.Adapters.SimpleContainer.Tests/ExtendedSimpleContainerAdapterTests.cs
@@ -8,43 +8,6 @@ namespace LogoFX.Client.Bootstrapping.Tests
     public class ExtendedSimpleContainerAdapterTests
     {
         [Fact]
-        public void Given_WhenDependencyIsRegisteredPerLifetimeAndDependencyIsResolved_ThenResolvedDependencyIsNotNull()
-        {
-            var container = new ExtendedSimpleContainerAdapter(new ExtendedSimpleContainer());
-            container.RegisterPerLifetime<ITestDependency, TestDependency>(() => TestLifetimeScopeProvider.Current);
-            TestLifetimeScopeProvider.Current = new TestObject();
-            var dependency = container.Resolve<ITestDependency>();
-
-            dependency.Should().NotBeNull();            
-        }
-
-        [Fact]
-        public void Given_WhenDependencyIsRegisteredPerLifetimeAndDependencyIsResolvedAndLifetimeChangesAndDependencyIsResolved_ThenResolvedDependenciesAreDifferent()
-        {
-            var container = new ExtendedSimpleContainerAdapter(new ExtendedSimpleContainer());
-            container.RegisterPerLifetime<ITestDependency, TestDependency>(() => TestLifetimeScopeProvider.Current);
-            TestLifetimeScopeProvider.Current = new TestObject();
-            var dependency1 = container.Resolve<ITestDependency>();
-            TestLifetimeScopeProvider.Current = new TestObject();
-            var dependency2 = container.Resolve<ITestDependency>();
-
-            dependency1.Should().NotBeSameAs(dependency2);            
-        }
-
-        [Fact]
-        public void Given_WhenDependencyIsRegisteredPerLifetimeAndDependencyIsResolvedAndLifetimeIsSetToNullAndDependencyIsResolved_ThenResolvedDependencyIsNull()
-        {
-            var container = new ExtendedSimpleContainerAdapter(new ExtendedSimpleContainer());
-            container.RegisterPerLifetime<ITestDependency, TestDependency>(() => TestLifetimeScopeProvider.Current);
-            TestLifetimeScopeProvider.Current = new TestObject();
-            var dependency = container.Resolve<ITestDependency>();
-            TestLifetimeScopeProvider.Current = null;
-            dependency = container.Resolve<ITestDependency>();
-
-            dependency.Should().BeNull();            
-        }
-
-        [Fact]
         public void Given_WhenDependencyIsRegisteredViaHandlerAndDependencyIsResolved_ThenResolvedDependencyIsNotNull()
         {
             var container = new ExtendedSimpleContainerAdapter(new ExtendedSimpleContainer());
